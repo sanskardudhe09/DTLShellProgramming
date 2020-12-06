1a6a37d8ad9fa0b271ff3cb358fa77142a4321fc
sanskardudhe09 <69566078+sanskardudhe09@users.noreply.github.com>
2020-12-06 12:35:49 +0530

    Add files via upload
    This is shell file for file concept

---
+++ coe.sh
@@ -0,0 +1,50 @@
+file="/home/user/DTL/coe.sh"
+
+if [ -r $file ]
+then
+   echo "File has read access"
+else
+   echo "File does not have read access"
+fi
+
+if [ -w $file ]
+then 
+   echo "File has write permission"
+else
+   echo "File does not have write permission"
+fi
+
+if [ -x $file ]
+then
+   echo "File has execute permission"
+else
+   echo "File does not have execute permission"
+fi
+
+if [ -f $file ]
+then
+   echo "File is an ordinary file"
+else
+   echo "File is a special file"
+fi
+
+if [ -d $file ]
+then
+   echo "File is a directory"
+else
+   echo "File is not a directory"
+fi
+
+if [ -s $file ]
+then
+   echo "File size is not zero"
+else
+   echo "File size is zero"
+fi
+
+if [ -e $file ]
+then
+   echo "File exists"
+else
+   echo "File does not  exists"
+fi
